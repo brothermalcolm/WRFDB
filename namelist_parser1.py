676f2a36065dd433dc58a28725ea48afedd3ca42
Malcolm Ng <malcolmng@alumni.purdue.edu>
2017-06-27 18:30:27 +0800

    regex for parsing keys

--- namelist_parser1.py
+++ namelist_parser1.py
@@ -9,6 +9,14 @@ import re
 
 fhand = open('namelist.input')
 names  = {}
+regex = '^\s(\S+)'
 
 for line in fhand:
-    print(line)
+    line = line.rstrip()
+#    print(line)
+    name = re.findall(regex, line)
+    if len(name) > 0 and not re.search('/', name[0]):
+        if re.search('^&', name[0]):
+            print('name_class is:', name[0])
+        else:
+            print('name_field is:', name[0])
