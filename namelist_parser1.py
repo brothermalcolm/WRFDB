ff914d7a291ca2ef5fbb0308319e3de0b874c9d4
Malcolm Ng <malcolmng@alumni.purdue.edu>
2017-06-28 11:43:11 +0800

    regex for parsing values

--- namelist_parser1.py
+++ namelist_parser1.py
@@ -9,14 +9,17 @@ import re
 
 fhand = open('namelist.input')
 names  = {}
-regex = '^\s(\S+)'
 
 for line in fhand:
     line = line.rstrip()
-#    print(line)
-    name = re.findall(regex, line)
+    #print(line)
+    name = re.findall('^\s(\S+)', line)
     if len(name) > 0 and not re.search('/', name[0]):
         if re.search('^&', name[0]):
             print('name_class is:', name[0])
         else:
             print('name_field is:', name[0])
+            value = re.findall('=\s*(.*)', line)
+            #value = re.findall('\s*(\S*?)\s*,', value[0])
+            value = value[0].split(',')
+            print('values are:', value)
