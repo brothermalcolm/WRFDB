852f4a78f9003aa1757278cf8f81cbc53a4e4c0a
Malcolm Ng <malcolmng@alumni.purdue.edu>
2017-06-29 15:18:56 +0800

    dict and list comprehension for all key-value pairs

--- namelist_parser1.py
+++ namelist_parser1.py
@@ -8,10 +8,13 @@ Created on Tue Jun 27 17:00:53 2017
 import re
 
 fhand = open('namelist.input')
+classes = []
+fields = []
+namelist = {}
 
 for line in fhand:
     line = line.rstrip()
-    name = re.findall('^\s(\S+)', line)
+    name = re.findall('^\s*(\S+)', line)
     if len(name) == 0:
         continue
     if re.search('/', name[0]):
@@ -19,11 +22,20 @@ for line in fhand:
     if re.search('^!', name[0]):
         continue
     if re.search('^&', name[0]):
+        print()
         print('name_class is:', name[0])
+        classe = name[0] 
+        classes.append(name[0])
     else:
-        print('name_field is:', name[0])
+        #print('name_field is:', name[0])
         value = re.findall('=\s*(.*)', line)
         value = value[0].split(',')
-        print('values are:', value)
-
-        
+        try:
+            value.remove('')
+        except ValueError:
+            pass
+        #print('values are:', value)
+        field = name[0]
+        fields.append(name[0])
+        print('%s : %s' % (field, value))
+        namelist[field] = value
