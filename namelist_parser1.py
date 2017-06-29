a0a51c09ea39e042031cd99c5472b95369085b38
Malcolm Ng <malcolmng@alumni.purdue.edu>
2017-06-29 13:22:29 +0800

    regex for parsing comments

--- namelist_parser1.py
+++ namelist_parser1.py
@@ -8,18 +8,22 @@ Created on Tue Jun 27 17:00:53 2017
 import re
 
 fhand = open('namelist.input')
-names  = {}
 
 for line in fhand:
     line = line.rstrip()
-    #print(line)
     name = re.findall('^\s(\S+)', line)
-    if len(name) > 0 and not re.search('/', name[0]):
-        if re.search('^&', name[0]):
-            print('name_class is:', name[0])
-        else:
-            print('name_field is:', name[0])
-            value = re.findall('=\s*(.*)', line)
-            #value = re.findall('\s*(\S*?)\s*,', value[0])
-            value = value[0].split(',')
-            print('values are:', value)
+    if len(name) == 0:
+        continue
+    if re.search('/', name[0]):
+        continue
+    if re.search('^!', name[0]):
+        continue
+    if re.search('^&', name[0]):
+        print('name_class is:', name[0])
+    else:
+        print('name_field is:', name[0])
+        value = re.findall('=\s*(.*)', line)
+        value = value[0].split(',')
+        print('values are:', value)
+
+        
